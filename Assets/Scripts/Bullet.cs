3fbe7702ec89493a40262c026f4fe78c30aa6b32
Unnecro <didac.p.fuster@gmail.com>
2015-10-14 19:15:59 +0200

    [TASK] - Destroy bullets when are not visible on screen.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -17,10 +17,19 @@ public class Bullet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = new Vector3(
-			transform.position.x + (30f * Time.deltaTime),
-			transform.position.y,
-			transform.position.z
-		);
+		if(
+			this.transform.position.x > Screen.width ||
+			this.transform.position.x < 0 ||
+			this.transform.position.y > Screen.height ||
+			this.transform.position.y < 0
+		){
+			DestroyImmediate(this);
+		} else {
+			this.transform.position = new Vector3(
+				transform.position.x + (30f * Time.deltaTime),
+				transform.position.y,
+				transform.position.z
+			);
+		}
 	}
 }
--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -6,7 +6,7 @@ public class HUDManager : MonoBehaviour {
 	private int world_units = 16;
 	public static float px_unit = 60;
 	public static float screen_units_height;
-	public static float limit_spacing_y = 0.75f;
+	public static float limit_spacing_y = 0.45f;
 
 	void Awake(){
 		px_unit = ((Screen.height / 2f) / Camera.main.orthographicSize);
