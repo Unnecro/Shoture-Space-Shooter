968b1b15ae4dd89bbfe66164ee0e05ec649a2a67
Unnecro <didac.p.fuster@gmail.com>
2015-10-17 03:33:01 +0200

    [RELEASE] v0.0-5 with moving enemies.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,17 @@ public class Enemy : MonoBehaviour {
 
 	private int health = 100;
 	private float scale = 0.01f;
+	private float velocity_x = 0f;
+	private float velocity_y = 0f;
+
 private int i = 0;
 	// Use this for initialization
 	void Start () {
 		this.transform.localScale = new Vector3(scale, scale, scale);
 		this.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+		
+		velocity_x = Random.Range(0.05f, 0.2f);
+		velocity_y = Random.Range(0.05f, 0.2f);
 	}
 	
 	// Update is called once per frame
@@ -17,41 +23,60 @@ private int i = 0;
 		if(this.transform.localScale.x < 1f){
 			this.transform.localScale += new Vector3(scale, scale, scale);	
 		}
+
+		if(
+			this.transform.position.x < 8 ||
+			this.transform.position.x > 15
+		){
+		
+			velocity_x = -velocity_x;
+		}
+
+		if(
+			this.transform.position.y < 0 ||
+			this.transform.position.y > 9
+		){
+			velocity_y = -velocity_y;
+		}
+
+		this.transform.position += new Vector3(velocity_x, velocity_y, 0f);
 		
 	}
 
 	void OnTriggerEnter2D(Collider2D collider){
-		Bullet bullet = collider.gameObject.GetComponent<Bullet>();
+		if(collider.gameObject.tag == "Bullet"){
+			Bullet bullet = collider.gameObject.GetComponent<Bullet>();
 
-		Color old_color = this.GetComponent<SpriteRenderer>().color;
-		Color new_color = new Color(
-			old_color.r,
-			old_color.g - (bullet.damage / 100f),
-			old_color.b - (bullet.damage / 100f),
-			old_color.a
-		);
-
-		this.GetComponent<SpriteRenderer>().color = new_color;
-
-		this.health -= bullet.damage;
-
-		if(this.health <= 0){
-			Instantiate(
-				this.gameObject,
-				new Vector3(
-					this.transform.position.x,
-					this.transform.position.y,
-					this.transform.position.z
-				),
-				Quaternion.identity
+			Color old_color = this.GetComponent<SpriteRenderer>().color;
+			Color new_color = new Color(
+				old_color.r,
+				old_color.g - (bullet.damage / 100f),
+				old_color.b - (bullet.damage / 100f),
+				old_color.a
 			);
 
+			this.GetComponent<SpriteRenderer>().color = new_color;
 
-			Destroy(gameObject);
+			this.health -= bullet.damage;
+
+			if(this.health <= 0){
+				Instantiate(
+					this.gameObject,
+					new Vector3(
+						this.transform.position.x,
+						this.transform.position.y,
+						this.transform.position.z
+					),
+					Quaternion.identity
+				);
+
+
+				Destroy(gameObject);
+
+			}
+
+			Destroy(collider.gameObject);
 
 		}
-
-		Destroy(collider.gameObject);
-
 	}
 }
